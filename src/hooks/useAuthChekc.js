943fca53d64f4783de7d6bfa98faa39796c47987
codernoyan <coder.noyan@gmail.com>
2023-05-18 23:55:32 +0600

    dark mode problem has been fixed

--- src/hooks/useAuthChekc.js
+++ src/hooks/useAuthChekc.js
@@ -2,19 +2,19 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { addUser } from "../fetures/usersApi/userSlice";
 
-export default function useAuthCheck (){
-    const [authCheck,setAuthCheck] = useState(false)
+export default function useAuthCheck() {
+    const [authCheck, setAuthCheck] = useState(false)
     const dispatch = useDispatch()
-    useEffect(()=>{
+    useEffect(() => {
         const localAuth = localStorage?.getItem("user");
-        if(localAuth){
+        if (localAuth) {
             const auth = JSON.parse(localAuth)
-            if(auth?.email){
+            if (auth?.email) {
                 dispatch(addUser(auth))
             }
         }
         setAuthCheck(true)
-    },[dispatch])
-   
+    }, [dispatch])
+
     return authCheck;
 }
