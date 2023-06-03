6d3fbf7b34f2d1758b2a42213390a41145a8dbbd
sh-mihad <sh.mihad.cse@gmail.com>
2023-06-03 18:25:08 +0600

    add doctors count down

--- src/compontents/AdminDashborad/AdminDashboradData/Cards.jsx
+++ src/compontents/AdminDashborad/AdminDashboradData/Cards.jsx
@@ -1,6 +1,9 @@
+import CountUp from "react-countup";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
+import { useGetDoctorsQuery } from "../../../fetures/doctorsApi/doctorsApi";
 
 const Cards = () => {
+  const { data: doctors } = useGetDoctorsQuery();
   return (
     <div className="my-6 border rounded-lg p-5 flex gap-4 items-center ">
       <div className="bg-cyan-300 p-5 text-white text-5xl rounded-lg">
@@ -8,7 +11,9 @@ const Cards = () => {
       </div>
       <div>
         <p className="text-lg text-gray-700 font-semibold">Total Doctors</p>
-        <h4 className="text-3xl text-gray-900 font-semibold">30</h4>
+        <h4 className="text-3xl text-gray-900 font-semibold">
+          <CountUp end={doctors?.length} duration={2} />
+        </h4>
       </div>
     </div>
   );
