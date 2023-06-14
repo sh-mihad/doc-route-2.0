9bdb1ab15a0f43eedff9f98d217f33a34ace6a10
sh-mihad <sh.mihad.cse@gmail.com>
2023-06-14 20:25:41 +0600

    add server link

--- src/fetures/api/apiSlice.js
+++ src/fetures/api/apiSlice.js
@@ -1,8 +1,8 @@
-import{createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const apiSlice = createApi({
    reducerPath:"api",
    baseQuery:fetchBaseQuery({
-    baseUrl: "http://localhost:5000",    
+    baseUrl: "https://doc-route-server.vercel.app",    
    }),
    endpoints:(builder)=>({})
 })
